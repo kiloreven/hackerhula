1493262b7bbf462bac6135b3d9b090106e1709f8
Lasse Karstensen <lasse.karstensen@gmail.com>
2016-01-09 11:27:53 +0100

    Improve deployment.

--- fabfile.py
+++ fabfile.py
@@ -16,21 +16,25 @@ def lint():
     cmd = ["pylint", "--rcfile=" + topdir + "/pylint.rc", "--errors-only", "*py"]
     local(" ".join(cmd))
 
-@hosts('webapp-hackerhula@blade.hackeriet.no')
+@hosts("webapp-hackerhula@blade.hackeriet.no")
 def deploy():
     run_testsuite()
-    # send upstream foerst, kjipt aa ha divergerende tre.
-    #local("git push origin HEAD")
+
+    local("git push origin HEAD")
 
     with settings(warn_only=True):
         if run("test -d hackerhula").failed:
             run("git clone https://github.com/hackeriet/hackerhula.git")
 
+    with settings(warn_only=True):
+        if run("test -d venv").failed:
+            run("virtualenv --system-site-packages venv")
+            run("venv/bin/pip install --quiet -r hackerhula/requirements.txt")
+
     with cd("hackerhula"):
         run("git pull")
         run("git checkout master")
-        #run('hackerhula/manage.py collectstatic --noinput')
-        run('hackerhula/manage.py syncdb --noinput')
-        run('hackerhula/manage.py migrate --noinput')
-        run('./deploy-crontab')
-    run('touch .reload')
+        #run("hackerhula/manage.py collectstatic --noinput")
+        run("../venv/bin/python hackerhula/manage.py migrate --noinput")
+        run("./deploy-crontab")
+    run("touch .reload")
