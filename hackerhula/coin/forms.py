9a54043a7fb7061f8245859b41def4db2cf30fd2
Lasse Karstensen <lasse.karstensen@gmail.com>
2016-01-09 10:54:40 +0100

    This spelling makes me sad.

--- hackerhula/coin/forms.py
+++ hackerhula/coin/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 
-
 class PayForm(forms.Form):
+    # XXX: Does this misspelling of amountt come from Stripe?
     amountt = forms.CharField(label='amountt', max_length=100)
     stripeToken = forms.CharField(label='stripeToken', max_length=100)
-
-
