6072907e733bb86adea202dabb5408b9eaf537f7
Lasse Karstensen <lasse.karstensen@gmail.com>
2016-01-09 10:55:54 +0100

    Final parts of the app name change

--- hackerhula/coin/tests.py
+++ hackerhula/coin/tests.py
@@ -4,6 +4,6 @@ from django.core.urlresolvers import reverse
 
 class ViewTests(TestCase):
     def test_auth(self):
-        for uri in [reverse("brusaccount")]:
+        for uri in [reverse("coinaccount")]:
             response = self.client.get(uri)
             self.assertEqual(response.status_code, 302)
