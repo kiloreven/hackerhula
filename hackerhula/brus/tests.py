3c194ecf57f409d2285e71de0c9ef5038e3190bc
Lasse Karstensen <lasse.karstensen@gmail.com>
2016-01-09 10:35:02 +0100

    Forgotten test file.

--- hackerhula/brus/tests.py
+++ hackerhula/brus/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+
+class ViewTests(TestCase):
+    def test_auth(self):
+        for uri in [reverse("brusaccount")]:
+            response = self.client.get(uri)
+            self.assertEqual(response.status_code, 302)
